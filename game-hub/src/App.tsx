504f5e8581891cbddd4b00ad52084f4cdd019763
Kamsiyochukwu Arinze <arinze_kamsiyochukwu@outlook.com>
2025-04-12 09:41:30 -0400

    built navbar

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,10 +1,22 @@
-import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Grid, GridItem, Show, useBreakpointValue } from "@chakra-ui/react"
+import NavBar from "./components/NavBar"
+
 
 
 function App(){
+  const isAboveMd = useBreakpointValue({ base: false, md: true })
+
   return(
-    <Button colorScheme="blue">Button</Button>
-    
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+    }}>
+      <GridItem area='nav'><NavBar/></GridItem>
+      <Show when={isAboveMd}>
+        <GridItem area='aside' bg='gold'>Aside</GridItem>
+      </Show>
+      <GridItem area='main' bg='dodgerblue'>Main</GridItem>
+    </Grid>
   )
 }
 
