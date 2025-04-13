18bff34e4b2535a82802c9be996016516572fa74
Kamsiyochukwu Arinze <arinze_kamsiyochukwu@outlook.com>
2025-04-12 20:36:18 -0400

    added loading state

---
+++ game-hub/src/components/GameCardSkeleton.tsx
@@ -0,0 +1,14 @@
+import { Skeleton, Card, SkeletonText} from '@chakra-ui/react'
+
+const GameCardSkeleton = () => {
+  return (
+    <Card.Root width="300px" borderRadius={10} overflow="hidden">
+        <Skeleton height="200px" />
+        <Card.Body>
+         <SkeletonText /> 
+        </Card.Body>
+    </Card.Root>
+  )
+}
+
+export default GameCardSkeleton
